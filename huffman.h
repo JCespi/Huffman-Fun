2e27669bb18c86344fd34f98fb16e393c83dd1df
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-03 00:49:51 -0500

    added bit information as well as a new field to a heap node

--- huffman.h
+++ huffman.h
@@ -1,12 +1,13 @@
 //API for creating a Huffman code; Jeacy Espinoza
 
-//a node in a huffman tree
+//a node in a huffman tree (N nodes total)
 typedef struct min_heap_node {
-	char letter;
-	unsigned int freq;
-	struct min_heap_node *left;
-	struct min_heap_node *right;
-} Heap_Node;
+	char letter;					//1  byte
+	unsigned int freq;				//4  bytes
+	unsigned int end_i;				//4 bytes
+	struct min_heap_node *left;		//8  bytes
+	struct min_heap_node *right;	//8  bytes
+} Heap_Node;						//25 bytes -> 25*N bytes total
 
 //returns a huffman tree by using a priority queue
 Heap_Node *build_huffman_tree(char *letters, unsigned int *freqs, int n);
