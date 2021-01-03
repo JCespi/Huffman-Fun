e98805a9ea661b5ca7aa04ecdd162db474e1a0fa
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-03 11:46:12 -0500

    feeds a frequency table to the huffman api

--- compress.c
+++ compress.c
@@ -2,18 +2,33 @@
 #include <stdlib.h>
 #include "huffman.h"
 
+unsigned *create_freq_table(void){
+    unsigned int *freq_table; //char-indexed frequency array
+
+    freq_table = calloc(N_CHARS, sizeof(int));
+
+    //sample input
+    freq_table['a'] = 45;
+    freq_table['b'] = 16;
+    freq_table['c'] = 13;
+    freq_table['d'] = 12;
+    freq_table['e'] = 9;
+    freq_table['f'] = 5;
+
+    return freq_table;
+}
+
 int main(int argc, char **argv){
     Heap_Node *root;
     Code_Word *codewords;
+    unsigned *freq_table;
 
-    char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' }; 
-    unsigned freq[] = { 5, 9, 12, 13, 16, 45 }; 
-	int size = 6;
-
-    codewords = create_huffman_code(&root, arr, freq, size, 1);
+    freq_table = create_freq_table();
+    codewords = create_huffman_code(&root, freq_table, N_CHARS, 1);
 
     free_huffman_tree(root);
 	free(codewords);
+    free(freq_table);
 
     return 0;
 }
