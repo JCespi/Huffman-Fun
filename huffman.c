ea1946b845edd39c7b6625c116630f394fa58512
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-03 20:49:17 -0500

    it's possible to toggle an efficient or inefficient huffman code now b/w why not
    determined by building a min or max heap, respectively

--- huffman.c
+++ huffman.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "huffman.h"
 
+//Macros
 #define INTERNAL_NODE_MARKER '$'
 #define ZERO_BIT 0
 #define ONE_BIT  1
@@ -17,9 +18,17 @@
 */
 
 //=============================================================
+//utility function to specify whether to create a min heap or max heap (efficient or inefficient)
+int comparator_utility(const void *node1, const void *node2, int efficient){
+	if (efficient)
+		return (((Heap_Node*)node1)->freq - ((Heap_Node*)node2)->freq);
+	else 
+		return (((Heap_Node*)node2)->freq - ((Heap_Node*)node1)->freq);
+}
+
 //comparator function to be used for heap creation and extraction
 int comparator(const void *node1, const void *node2){
-	return (((Heap_Node*)node1)->freq - ((Heap_Node*)node2)->freq);
+	return comparator_utility(node1, node2, 1);
 }
 
 Heap_Node *create_huff_node(char letter, unsigned int freq){
