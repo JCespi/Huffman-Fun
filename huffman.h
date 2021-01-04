6d62e83d64f6fde18e207165c20d087d6cba738d
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-03 19:27:44 -0500

    the huffman tree node is now only defined here

--- huffman.h
+++ huffman.h
@@ -3,6 +3,14 @@
 
 #define N_CHARS 256
 
+//a node in a huffman tree (N nodes total)
+typedef struct min_heap_node {
+	unsigned int letter;		    //1  byte
+	unsigned int freq;				//4  bytes
+	struct min_heap_node *left;		//8  bytes
+	struct min_heap_node *right;	//8  bytes
+} Heap_Node;						//25 bytes -> 21*N bytes total
+
 //contains the codeword (decimal) and the num of bits
 typedef struct code_word {
 	unsigned int code_d;		//codeword in decimal notation
