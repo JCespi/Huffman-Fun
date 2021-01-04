d7a702dccd8977d915b98666e0e7d398db8d06ee
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-03 19:50:33 -0500

    continued making blurb look cooler

--- min_heap.h
+++ min_heap.h
@@ -1,6 +1,7 @@
 //API for creating a special minimum heap; Jeacy Espinoza
 
-/* _______________________Instructions To Use Functions Below___________________________
+/* _____________________________________________________________________________________
+ * |______________________Instructions To Use Functions Below__________________________|
  * | For the comparator parameter, int return value meaning:						   |	
  * |	  1. (< 0) means that the first parameter is less than the second parameter	   |	
  * |      2. (= 0) means that the first parameter is equal to the second parameter	   |
