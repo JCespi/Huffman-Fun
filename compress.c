068ca08d345dedb618c7614a19f2e1780dc66ebd
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-04 11:42:46 -0500

    deleted sample input

--- compress.c
+++ compress.c
@@ -43,23 +43,6 @@ void decode(){
     ;
 }
 
-unsigned *create_freq_table(void){
-    unsigned int *freq_table; //char-indexed frequency array
-
-    freq_table = calloc(N_CHARS, sizeof(int));
-
-    //sample input
-    freq_table['a'] = 45;
-    freq_table['b'] = 16;
-    freq_table['c'] = 13;
-    freq_table['d'] = 12;
-    freq_table['e'] = 9;
-    freq_table['f'] = 5;
-    freq_table['j'] = 46;
-
-    return freq_table;
-}
-
 int main(int argc, char **argv){
     encode();
 
