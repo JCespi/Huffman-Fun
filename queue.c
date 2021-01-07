5b283cd2016495385c5b55b970f105177b1ad843
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-07 14:29:58 -0500

    added queue code to be used to print huffman table in sorted order

---
+++ queue.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "queue.h"
+
+//used to create a node in the queue
+Q_Node *new_node(void *key){
+    Q_Node *new = malloc(sizeof(Q_Node));
+    new->key = key;
+    new->next = NULL;
+    return new;
+}
+
+Queue *create_queue(void){
+    Queue *queue = malloc(sizeof(Queue));
+    queue->len = 0;
+    queue->front = queue->rear = NULL;
+    return queue;
+}
+
+void enqueue(Queue *queue, void *key){
+    Q_Node *new;
+
+    if (!queue)
+        return;
+
+    new = new_node(key);
+    queue->len++;
+
+    if (!queue->rear)
+        queue->front = queue->rear = new;
+    else {
+        queue->rear->next = new;
+        queue->rear = new;
+    }
+}
+
+void *dequeue(Queue *queue){
+    Q_Node *front_node;
+
+    if (!queue)
+        return;
+    
+    front_node = queue->front;
+    queue->front = queue->front->next;
+    queue->len--;
+
+    if (!queue->front)
+        queue->rear = NULL;
+    
+    return front_node;
+}
+
+unsigned len(Queue *queue){
+    if (!queue)
+        return 0;
+
+    return queue->len;
+}
+
+void free_queue(Queue *queue){
+    Q_Node *front, *temp;
+
+    if (!queue)
+        return;
+    
+    front = queue->front;
+    while (front){
+        temp = front;
+        front = front->next;
+
+        free(temp);
+    }
+
+    free(queue);
+}
---
+++ queue.h
@@ -0,0 +1,27 @@
+//a queue node (linked list node)
+typedef struct q_node {
+    void *key;
+    struct q_node *next;
+} Q_Node;
+
+//the linked list queue
+typedef struct queue {
+    unsigned len;
+    Q_Node *front;
+    Q_Node *rear;
+} Queue;
+
+//creates an empty queue
+Queue *create_queue(void);
+
+//adds a given key to the end of the queue
+void enqueue(Queue *queue, void *key);
+
+//removes and returns key at the front of the queue
+void *dequeue(Queue *queue);
+
+//returns the number of items in the queue
+unsigned len(Queue *queue);
+
+//frees the queue
+void free_queue(Queue *queue);
