15d7252c60047da3bd9173f9ff0bb6ae166580d2
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-08 10:55:35 -0500

    moved main() and TO-DO to main.c

--- compress.c
+++ compress.c
@@ -7,21 +7,6 @@
 #include "table.h"
 #include "queue.h"
 
-/* TO-DO
- * fix there being a little garbage that gets encoded
- *          - if you don't rewind, you can see the garbage itself
- *          - happened after making frequencies decimals and dedicating build_freq_table() to that
- * check for memory leaks by compiling on the zoo
- * add bit fields
- * add more detailed comments to header file functions. (@param, @return)
- * generalize what we can huffman encode (like being able to pass a frequency array of anything/any size)
- * add error handling to main()
- * maybe make a main.c that has the main() that calls on encode and decode (defined in some .h)
- * for dumping the table:
- *      - handle multiple sources of redirection with a default
- *      - add functions to find appropriate sizes for column widths
-*/
-
 //a high bit followed by a byte signifies a leaf node with following letter
 void send_huff_tree(Heap_Node *root){
     if (is_leaf(root)){
@@ -246,35 +231,4 @@ void decode(void){
     }
 
     free_huffman_tree(root);
-}
-
-int main(int argc, char **argv){
-    char *path, *exectuable;
-    int i, dump;
-    
-    //get executable name
-    path = argv[0];
-    exectuable = &path[strlen(path) - 6];
-
-    if (strcmp("decode", exectuable) == 0){
-        if (argc > 1)
-            DIE("%s", "decode: accepts no options");
-
-        decode();
-    } else {
-        dump = 0;   //default dump option is false
-        i = 1;
-
-        while (i < argc){
-            if (strcmp(argv[i], "-d") == 0){
-                dump = 1;
-                i++;
-            } else 
-                DIE("%s", "encode: unknown flag");
-        }
-
-        encode(dump);
-    }
-
-    return 0;
 }
--- compress.h
+++ compress.h
@@ -1,6 +1,8 @@
 //Macros
 #define DIE(format, ...) fprintf(stderr, format "\n", __VA_ARGS__), exit(EXIT_FAILURE)
 
+//read file to form frequency table, generate huffman code and tree, send tree and codewords
 void encode(int dump);
 
+//read the huffman tree, read codewords and output corresponding letters
 void decode(void);
