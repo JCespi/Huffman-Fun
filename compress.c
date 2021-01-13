62080d6feb9dfd380d2185762a4dbd378fa02162
Jeacy Espinoza <jeacyespinoza@Jeacys-MBP.home>
2021-01-12 20:16:59 -0500

    huge commit where an EOC (end of code) entry exists in the frequency table, gets added to the huffman tree, and ultimately helps decode() end its loop. required changing format of send_tree() -> and receive_tree()

--- compress.c
+++ compress.c
@@ -11,6 +11,9 @@
 //Macros
 #define BIT 1
 #define BYTE 8
+#define INTERNAL 0 //signifies an internal node
+#define LEAF     1 //signifies a leaf node
+#define EOC 256    //signifies a 256th character that signals an end to transmission
 
 //=============================Dump_Functions=================================
 //helper function that uses table print functions
@@ -22,7 +25,7 @@ void process_node(Heap_Node *node, Code_Word *codewords, unsigned n_cols){
     //container to hold the strs to be passed to table.h function
 	row_strs = malloc(sizeof(char*) * n_cols);
 
-    if (is_leaf(node)){
+    if (is_leaf(node) && node->letter != EOC){
         //generate data to be converted to strings
         letter = node->letter;
         freq = node->freq;
@@ -58,12 +61,12 @@ void free_str_container(char **container, int n){
 }
 
 //initialize tile and column length arrays
-void init_metadata(Code_Word *codewords, char **col_titles, unsigned *col_lens, unsigned n_cols){
+void init_metadata(Code_Word *codewords, char **col_titles, unsigned *col_lens, unsigned n_cols, unsigned n){
     unsigned i, min_bit_col, min_dig_col, padding;
 
     padding = 2; //(padding)
-    min_bit_col = find_max(codewords, 1) + padding;          //minimum size of columns that depend on bits
-    min_dig_col = get_n_digits(find_max(codewords, 0)) + padding; //min size of cols that depend on digits
+    min_bit_col = find_max(codewords, n, 1) + padding;          //minimum size of columns that depend on bits
+    min_dig_col = get_n_digits(find_max(codewords, n, 0)) + padding; //min size of cols that depend on digits
 
     //fill in column titles string array as well as the column lengths array
     char *str_arr[] = {"Letter", "Freq (%)", "Code (2)", "Code (10)", "Nbits"};
@@ -79,7 +82,7 @@ void init_metadata(Code_Word *codewords, char **col_titles, unsigned *col_lens, 
 }
 
 //used to interface with functions in table.h to produce a table
-void dump_input_info(char *dmp_file, Heap_Node *root, Code_Word *codewords, float comp){
+void dump_input_info(char *dmp_file, Heap_Node *root, Code_Word *codewords, float comp, unsigned n){
 	unsigned n_cols, *col_lens;
 	char *str_buffer, **col_titles;
     Heap_Node *min_node;
@@ -94,7 +97,7 @@ void dump_input_info(char *dmp_file, Heap_Node *root, Code_Word *codewords, floa
 	n_cols = 5;
     col_lens = calloc(n_cols, sizeof(int));
     col_titles = malloc(sizeof(char*) * n_cols);
-    init_metadata(codewords, col_titles, col_lens, n_cols);
+    init_metadata(codewords, col_titles, col_lens, n_cols, n);
 
     //call on appropriate functions to set up the table
 	set_dump_file(fp);
@@ -104,7 +107,7 @@ void dump_input_info(char *dmp_file, Heap_Node *root, Code_Word *codewords, floa
     asprintf(&str_buffer, "Compression = %0.2f%%", comp);
     print_pretty_centered(str_buffer);
     free(str_buffer);
-	asprintf(&str_buffer, "Average Length of codewords = %0.2f", find_avg_len(codewords));
+	asprintf(&str_buffer, "Average Length of codewords = %0.2f", find_avg_len(codewords, n));
 	print_pretty_centered(str_buffer);
     free(str_buffer);
 
@@ -141,10 +144,12 @@ void dump_input_info(char *dmp_file, Heap_Node *root, Code_Word *codewords, floa
 
 //=============================*code_Functions================================
 //reads chars from stdin, building a frequency table. fills variable passed by reference
-unsigned build_freq_table(float *freq_table){
+unsigned build_freq_table(float *freq_table, unsigned table_size){
     unsigned i, tot_n_chars, ch;
+    float smallest_freq;
 
     tot_n_chars = 0;
+    smallest_freq = 100.0;
 
     //receive characters from stdin
     while ((ch = getchar()) != EOF){
@@ -153,9 +158,18 @@ unsigned build_freq_table(float *freq_table){
     }
     
     //update the frequencies to be percentages
-    for (i=0; i < N_CHARS; i++)
-        freq_table[i] = (freq_table[i] / tot_n_chars) * 100;
-    
+    for (i=0; i < table_size; i++){
+        if (freq_table[i]){
+            freq_table[i] = (freq_table[i] / tot_n_chars) * 100;
+
+            if (freq_table[i] < smallest_freq)
+                smallest_freq = freq_table[i];
+        }
+    }
+
+    //add the special "EOF" character to the table with the smallest frequency
+    freq_table[EOC] = smallest_freq;
+        
     //return the total number of bits in the input file
     return tot_n_chars * BYTE;
 }
@@ -165,21 +179,28 @@ float compute_compression(unsigned n_bits_in, unsigned n_bits_out){
     return (((float)n_bits_in - (float)n_bits_out) / (float)n_bits_in) * 100;
 }
 
-//a high bit followed by a byte signifies a leaf node with following letter.returns num of output bytes
+//a low bit signifies an internal node.
+//a high bit followed by a low bit followed by a byte signifies leaf node and letter
+//a high bit followed by a high bit signifies an EOC leaf node
+//returns num of output bytes
 unsigned send_huff_tree(Heap_Node *root){
     if (is_leaf(root)){
-        put_bits(BIT, ONE_BIT);
-        put_bits(BYTE, root->letter);
-        return BIT + BYTE;
+        put_bits(BIT, LEAF);
+        put_bits(BIT, root->letter == EOC);
+
+        if (root->letter != EOC)
+            put_bits(BYTE, root->letter);
+
+        return BIT + BIT + BYTE;
     } else {
-        put_bits(BIT, ZERO_BIT);
+        put_bits(BIT, INTERNAL);
         return BIT + send_huff_tree(root->left) + send_huff_tree(root->right);
     }
 }
 
 //read file to form frequency table, generate huffman code and tree, send tree and codewords
 void encode(int dump, char *dmp_file){
-    unsigned n_bits_in, n_bits_out, ch;
+    unsigned n_bits_in, n_bits_out, ch, table_size;
     Code_Word *codewords;
     float *freq_table;
     Heap_Node *root;
@@ -188,11 +209,12 @@ void encode(int dump, char *dmp_file){
     n_bits_in = n_bits_out = 0;
 
     //build the frequency table
-    freq_table = calloc(N_CHARS, sizeof(float));
-    n_bits_in += build_freq_table(freq_table);
-
+    table_size = N_CHARS + 1;
+    freq_table = calloc(table_size, sizeof(float));
+    n_bits_in += build_freq_table(freq_table, table_size);
+    
     //create the huffman code
-    codewords = create_huffman_code(&root, freq_table);
+    codewords = create_huffman_code(&root, freq_table, table_size);
 
     //send the huffman tree
     n_bits_out += send_huff_tree(root);
@@ -203,13 +225,16 @@ void encode(int dump, char *dmp_file){
         put_bits(codewords[ch].n_bits, codewords[ch].code_d);
         n_bits_out += codewords[ch].n_bits;
     }
+
+    //output the codeword that signals the end
+    put_bits(codewords[EOC].n_bits, codewords[EOC].code_d);
         
     //flush any bits caught in buffer
     n_bits_out += flush_bits();
 
     //dump the tree by performing breadth-first search
     if (dump)
-        dump_input_info(dmp_file, root, codewords, compute_compression(n_bits_in, n_bits_out));
+        dump_input_info(dmp_file, root, codewords, compute_compression(n_bits_in, n_bits_out), table_size);
 
     //free the tree, the codeword table, and the frequency table
     free_huffman_tree(root);
@@ -221,13 +246,16 @@ void encode(int dump, char *dmp_file){
 Heap_Node *receive_huff_tree(void){
     Heap_Node *root;
 
-    if (get_bits(BIT) == ZERO_BIT){   //internal node
+    if (get_bits(BIT) == INTERNAL){   //internal node
         root = create_huff_node(INTERNAL_NODE_MARKER, 0);
         root->left = receive_huff_tree();
         root->right = receive_huff_tree();
         return root;
     } else {                        //leaf node
-        return create_huff_node(get_bits(BYTE), 0);
+        if (get_bits(BIT))
+            return create_huff_node(EOC, 0);
+        else
+            return create_huff_node(get_bits(BYTE), 0);
     }
 }
 
@@ -245,6 +273,9 @@ void decode(void){
 
         //a leaf node (a letter) found
         if (is_leaf(ptr)){
+            if (ptr->letter == EOC)
+                break;
+
             putchar(ptr->letter);
             ptr = root; //reset to root of tree
         }
